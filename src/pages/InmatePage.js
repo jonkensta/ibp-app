c547ac862ed9481f489f77493b4eebe81d5ac086
Jonathan Starr <jstarr@carnegietechnologies.com>
2020-02-22 23:50:12 -0600

    Don't need to await JSON data for DELETE methods

--- src/pages/InmatePage.js
+++ src/pages/InmatePage.js
@@ -46,33 +46,34 @@ export default (props) => {
   );
 
   const fetchHelper = async (url, method, data=undefined) => {
-    const response = await fetch(url, {
+    return await fetch(url, {
       method: method,
       credentials: "same-origin",
       body: data && JSON.stringify(data),
       headers: {'Content-Type': 'application/json'},
     });
-
-    const json = await response.json();
-    const ok = response.ok;
-    return [json, ok]
   };
 
   const RequestsCardContent = () => {
 
     const handleRequestAdd = async (newRequest) => {
       const url = `${props.urlBase}/request/${jurisdiction}/${id}`;
-      return await fetchHelper(url, 'POST', newRequest);
+      const response = await fetchHelper(url, 'POST', newRequest);
+      const json = await response.json();
+      return [json, response.ok];
     };
 
     const handleRequestUpdate = async (oldRequest, newRequest) => {
       const url = `${props.urlBase}/request/${jurisdiction}/${id}/${oldRequest.index}`;
-      return await fetchHelper(url, 'PUT', newRequest);
+      const response = await fetchHelper(url, 'PUT', newRequest);
+      const json = await response.json();
+      return [json, response.ok];
     };
 
     const handleRequestDelete = async (oldRequest) => {
       const url = `${props.urlBase}/request/${jurisdiction}/${id}/${oldRequest.index}`;
-      return await fetchHelper(url, 'DELETE');
+      const response = await fetchHelper(url, 'DELETE');
+      return response.ok;
     };
 
     return (
@@ -91,18 +92,23 @@ export default (props) => {
   const CommentsCardContent = () => {
 
     const handleCommentAdd = async (newComment) => {
-      const url = `${props.urlBase}/comment`;
-      return await fetchHelper(url, 'POST', newComment);
+      const url = `${props.urlBase}/comment/${jurisdiction}/${id}`;
+      const response = await fetchHelper(url, 'POST', newComment);
+      const json = await response.json();
+      return [json, response.ok];
     };
 
     const handleCommentUpdate = async (oldComment, newComment) => {
       const url = `${props.urlBase}/comment/${jurisdiction}/${id}/${oldComment.index}`;
-      return await fetchHelper(url, 'PUT', newComment);
+      const response = await fetchHelper(url, 'PUT', newComment);
+      const json = await response.json();
+      return [json, response.ok];
     };
 
     const handleCommentDelete = async (oldComment) => {
       const url = `${props.urlBase}/comment/${jurisdiction}/${id}/${oldComment.index}`;
-      return await fetchHelper(url, 'DELETE');
+      const response = await fetchHelper(url, 'DELETE');
+      return response.ok;
     };
 
     return (
